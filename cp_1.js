b277dcabe45772546cc00a52c02d27d59faa523d
Justin Wong <jcwong5@usf.edu>
2025-10-18 11:03:30 -0400

    Prevents submission if the field is empty and shows a validation message

--- cp_1.js
+++ cp_1.js
@@ -1,6 +1,8 @@
 const usernameInput = document.getElementById("username");
 const emailInput = document.querySelector("#email");
 const commentsInput = document.querySelector("#comments");
+const submitBtn = document.getElementById("submitButton")
+
 
 commentsInput.addEventListener("input", () => {
     console.log("Characters:", commentsInput.value.length);
@@ -12,4 +14,13 @@ commentsInput.addEventListener("mouseover", () => {
 });
 commentsInput.addEventListener("mouseout", () => {
     console.log("You are now outside the comments box!")
+});
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const comments = commentsInput.value
+    if (comments === "") {
+        alert("Please leave a comment.");
+    }
+    return;
 });
